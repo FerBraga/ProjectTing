6fa84338b4f749be9d21b6f35978bcf7473301b3
fer braga <augustofbg@gmail.com>
2022-12-05 08:52:12 -0300

    feat: req 02 ok - txtimporter

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+
+    try:
+        with open(path_file, mode='r') as file:
+
+            lines = [row.strip() for row in file]
+
+        return lines
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
