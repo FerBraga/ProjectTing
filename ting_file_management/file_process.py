5bbad7d8a6e613cd94ac5473e26845c752b4491d
fer braga <augustofbg@gmail.com>
2022-12-06 11:53:49 -0300

    feat: req 02

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+
+    for i in instance._data:
+        if path_file == i['nome_do_arquivo']:
+            return 'path already used'
+
+    lines = txt_importer(path_file)
+
+    lines_qtty = len(lines)
+
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": lines_qtty,
+        "linhas_do_arquivo": lines
+    }
+
+    instance.enqueue(new_dict)
+
+    sys.stdout.write(f"{new_dict}")
 
 
 def remove(instance):
