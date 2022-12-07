477118a2dd0e4c39a92b8e917b48249672893b9a
fer braga <augustofbg@gmail.com>
2022-12-07 07:54:14 -0300

    feat req 06

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -3,8 +3,6 @@ def exists_word(word, instance):
     for line in instance._data:
         if word not in line:
             return []
-        else:
-            
 
 
 def search_by_word(word, instance):
