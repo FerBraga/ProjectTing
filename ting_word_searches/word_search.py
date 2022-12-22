97247178049185e435b460a107e5ff06f46fdd71
fer braga <augustofbg@gmail.com>
2022-12-22 06:50:40 -0300

    ultimo commit para subir

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -3,6 +3,12 @@ def exists_word(word, instance):
     for line in instance._data:
         if word not in line:
             return []
+        else:
+            {
+                "palavra": word,
+                "arquivo": instance._data['nome_do_arquivo'],
+                "ocorrencias": fsdfs
+            }
 
 
 def search_by_word(word, instance):
